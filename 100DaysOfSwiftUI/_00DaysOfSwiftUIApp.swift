1ced4d00572ce982fefddc842f6dc6d6d805c7b0
Tarikvillalobos <tarik.villalobos@gmail.com>
2023-04-17 16:12:39 -0300

    Initial Commit

---
+++ 100DaysOfSwiftUI/_00DaysOfSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  _00DaysOfSwiftUIApp.swift
+//  100DaysOfSwiftUI
+//
+//  Created by Tarik Villalobos on 17/04/23.
+//
+
+import SwiftUI
+
+@main
+struct _00DaysOfSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
